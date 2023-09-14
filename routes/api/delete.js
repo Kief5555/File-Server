5c5c31bd06ec8d8ad702f801cdd40591e132354e
Kief <67562560+Kief5555@users.noreply.github.com>
2023-09-14 15:31:26 -0700

    Missing authendication

--- routes/api/delete.js
+++ routes/api/delete.js
@@ -1,14 +1,16 @@
 const path = require("path");
 const fs = require("fs");
+const mime = require("mime-types");
 const bcrypt = require('bcrypt'); // Import bcrypt
-const config = require('../../config/config.json');
+
+const publicUploadPath = path.join(__dirname, "..", "..", "files", "public");
 
 module.exports = {
-  Name: "API - Delete File",
-  Route: "/api/delete/",
-  Method: "POST",
-  Log: "File", // Set to 'File', 'Console', or 'Null'
-  Sqlite: "users.sqlite",
+  Name: "API - Create Directory",
+  Route: "/api/createDir/*",
+  Method: "GET",
+  Log: null, // Set to 'File', 'Console', or 'Null'
+  Sqlite: null,
   /**
    * Handle the request.
    * @param {import('express').Request} req - The request object.
@@ -18,22 +20,20 @@ module.exports = {
   async handle(req, res, db) {
     // Authorize user (username and password in headers)
     // If not authorized, return 401
-    if(config.Authentication == false ){
-      res.status(401).json({ message: 'Authentication failed' });
-    }
+
     const username = req.headers.username;
     const password = req.headers.password;
 
     // Retrieve the user's hashed password from the database
-    db.get("SELECT * FROM users WHERE username = ?", [username], (err, row) => {
+    db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
       if (err) {
         console.error(err);
-        res.status(500).send("Internal server error");
+        res.status(500).send('Internal server error');
         return;
       }
 
       if (!row) {
-        res.status(401).send("Unauthorized");
+        res.status(401).send('Unauthorized');
         return;
       }
 
@@ -41,52 +41,25 @@ module.exports = {
       bcrypt.compare(password, row.password, (compareErr, result) => {
         if (compareErr) {
           console.error(compareErr);
-          res.status(500).send("Authentication error");
+          res.status(500).send('Authentication error');
           return;
         }
 
         if (!result) {
-          res.status(401).send("Authentication failed");
+          res.status(401).send('Authentication failed');
           return;
         }
 
-        // Once authenticated, continue with file deletion logic
-        const folder = req.query.folder || "public";
-        const filename = req.query.file;
+        // Once authenticated, continue with directory creation logic
+        const dirPath = path.join(publicUploadPath, req.params[0]);
 
-        if (!filename) {
-          res.status(500).send("Cannot find file");
-          return;
-        }
-
-        const publicUploadPath = path.join(__dirname, "../../files/", folder);
-        const filePath = path.join(publicUploadPath, filename);
-
-        fs.access(filePath, fs.constants.F_OK, (err) => {
-          if (err) {
-            res.status(500).send("File or folder does not exist");
+        fs.mkdir(dirPath, { recursive: true }, (mkdirErr) => {
+          if (mkdirErr) {
+            res.status(500).send('Error creating directory');
             return;
           }
 
-          if (filename.endsWith("/")) {
-            // If the filename ends with a '/', it's a folder
-            fs.rmdir(filePath, { recursive: true }, (error) => {
-              if (error) {
-                res.status(500).send("Failed to delete folder");
-              } else {
-                res.json({ message: "Folder deleted successfully" });
-              }
-            });
-          } else {
-            // Otherwise, it's a file
-            fs.unlink(filePath, (error) => {
-              if (error) {
-                res.status(500).send("Failed to delete file");
-              } else {
-                res.json({ message: "File deleted successfully" });
-              }
-            });
-          }
+          res.json({ message: 'Directory created successfully' });
         });
       });
     });
