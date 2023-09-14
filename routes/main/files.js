d09b7aab6ef626f25d13ea51cfd28bfbf47947fa
Kief <67562560+Kief5555@users.noreply.github.com>
2023-09-14 07:49:27 -0700

    CVE Test

--- routes/main/files.js
+++ routes/main/files.js
@@ -32,10 +32,11 @@ module.exports = {
     const requestPath = path.normalize(req.params[0]);
     const filePath = path.join(publicUploadPath, requestPath);
     //check if the path starts with /files/public, if not the user could be trying to access the linux file system
-    if (requestPath.startsWith("/" || ".")) {
+    if (requestPath.startsWith("/" || "." || "*")) {
       res.status(403).sendFile(path.join(__dirname, "..", "..", "public", "403.html"));
       return;
     }
+    
     // Check if requested path is a directory
     if (!fs.existsSync(filePath)) {
       res.status(404).sendFile(path.join(__dirname, "..", "..", "public", "FNF.html"));
