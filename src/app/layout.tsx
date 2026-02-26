e9f0c318680aec68217560a7c5e226baffbeab02
Kiefer <kieferlin1001@gmail.com>
2026-02-25 16:53:54 -0800

    Hate flashbangs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
+          scriptProps={{ 'data-cfasync': 'false' }}
         >
           {children}
           <Toaster />
